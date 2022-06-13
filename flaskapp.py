a7abb6ab7d787ecf57dae92ae93d4b3fe6605e90
EndriVeizaj <endri.veizaj@gmail.com>
2022-06-13 11:53:02 +0200

    update

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloWorld():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ['GET'])
+def say_hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0', port = 5000)
